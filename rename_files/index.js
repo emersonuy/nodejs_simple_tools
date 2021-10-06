ca16b2981f4f1c47d597eff16d591c40bc0f184a
Eliot Emerson Uy <emersonuy@gmail.com>
2021-10-06 14:53:06 +0800

    Modified to rename only selected files. Add shortcut to shell:sendto to run this nodejsapp.

--- rename_files/index.js
+++ rename_files/index.js
@@ -1,50 +1,36 @@
 const fs = require("fs");
+const path = require("path");
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-
 rl.question("Enter filename: ", function(new_filename) {
-    fs.readdir(process.argv[2], {withFileTypes: true}, (err, files) => {
-        let cnt = 1;
+    
+    let i = 2;
+    let cnt = 1;
 
-        if (err) {
-            console.log(err);
+    for (i=process.argv.length-1; i>=2; i--) {
+
+        if (fs.lstatSync(process.argv[i]).isFile() === true) {
+            let og_filename = process.argv[i];
+            let extension = "";
+
+            if (og_filename.includes(".")) {
+                tokens = og_filename.split(".");
+                extension = tokens[tokens.length-1];
+            }
+
+            console.log(og_filename, path.dirname(process.argv[i]));
+
+            fs.renameSync(`${og_filename}`, `${path.dirname(og_filename)}/${new_filename}_${cnt++}.${extension}`);
         }
-        else {
-            files.forEach((file)=> {
-                let extension = "";
+    }
 
-                if (file.isDirectory()) {
-                    console.log(file.name);
-                    return;
-                }
-
-                let og_filename = file.name;
-
-                if (og_filename.includes(".")) {
-                    tokens = og_filename.split(".");
-                    extension = tokens[tokens.length-1];
-                }
-
-                console.log(`Renaming \"${og_filename}\" to \"${new_filename}_${cnt}.${extension}\"`);
-
-                fs.rename(`${process.argv[2]}/${og_filename}`, `${process.argv[2]}/${new_filename}_${cnt++}.${extension}`, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                    }
-                });
-            })
-        }
-
-        rl.question("Press any key to exit", function(key) {
-            rl.close();
-        });
-    })
+    rl.question("Press [Enter] to exit", function(key) {
+        rl.close();
+    });
 });
 
 
